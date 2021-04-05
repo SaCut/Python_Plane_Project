5102fa4d58c582ed5ef9cc29afa6384b83ab2fc7
OneOverCosine <TheCandyDragon@gmail.com>
2021-04-05 12:25:59 +0100

    Updated Aircraft.py, Plane.py, Helicopter.py

--- aircraft/Helicopter.py
+++ aircraft/Helicopter.py
@@ -1,1 +1,5 @@
-# Helicopter class (Isobel)
+from Aircraft import Aircraft
+
+class Helicopter(Aircraft):
+    def __init__(self, aircraft_id, flight_capacity):
+        super().__init__(aircraft_id, flight_capacity)

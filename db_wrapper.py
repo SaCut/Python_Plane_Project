ca6ebcc327efc695d992baee40c7d057632316cf
Ben <benranson5@gmail.com>
2021-04-05 12:08:36 +0100

    added db connection code and pyodbc

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,12 +1,20 @@
+import pyodbc
 
 
 class DbWrapper:
+
     def __init__(self):
         file_lines = open("server_info.cfg", "r").readlines()
         self.ip = file_lines[0].strip("\n")
         self.uname = file_lines[1].strip("\n")
         self.password = file_lines[2].strip("\n")
         self.DbName = file_lines[3].strip("\n")
+
+        self.connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};' +
+                                         f'SERVER={self.ip};DATABASE={self.DbName};'
+                                         f'UID={self.uname};PWD={self.password}')
+
+        self.cursor = self.connection.cursor()
 
 
 db = DbWrapper()

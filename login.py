0d7967c931fd649e35b7b7347cc52d62cba37eeb
Saverio <SaCut@users.noreply.github.com>
2021-04-06 20:11:06 +0100

    still working on login.py

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ import hashlib, os
 class Login:
     def __init__(self):
         self.database = DbWrapper()
-        self.cursor = self.database.cursor
+        # self.database.cursor = self.database.cursor
 
     def attempt_login(self):
         attempts = 5
@@ -16,20 +16,20 @@ class Login:
             print(f"Attempts left: {str(attempts)}")
             username = input("Insert user name: ")
             
-            all_unames = self.cursor.execute("SELECT username FROM login_credentials").fetchall()
+            all_unames = self.database.cursor.execute("SELECT username FROM login_credentials").fetchall()
 
             if username in all_unames:
 
                 # retrieving the salt from the database
-                get_salt = self.cursor.execute(f"SELECT salt FROM login_credentials WHERE username = {username}")
+                get_salt = self.database.cursor.execute(f"SELECT salt FROM login_credentials WHERE username = {username}")
 
                 # retrieving the hashed password from the database
-                get_pswd = self.cursor.execute(f"SELECT password FROM login_credentials WHERE username = {username}")
+                get_pswd = self.database.cursor.execute(f"SELECT password FROM login_credentials WHERE username = {username}")
 
                 run_pswd = input("Insert password: ")
 
                 # hashing the user-given password
-                check_pswd = hashlib.pbkdf2_hmac('sha256', run_pswd.encode('utf-8'), user.["salt"], 1000)
+                check_pswd = hashlib.pbkdf2_hmac('sha256', run_pswd.encode('utf-8'), user["salt"], 1000)
 
                 if check_pswd == get_pswd:
                     return True # returns True when the password is correct
@@ -42,20 +42,22 @@ class Login:
 
         return False # return False if the correct password has not been inserted
 
-    def new_account(self, username=input("Insert new user name: "), password=input("Insert new password: ")):
+    def new_account(self):
+        username = input("Insert new user name: ")
+        password = input("Insert new password: ")
+        salt = os.urandom(10)
 
-        all_unames = self.cursor.execute("SELECT username FROM login_credentials").fetchall()
+        all_unames = self.database.cursor.execute("SELECT username FROM login_credentials").fetchall()
 
         if username not in all_unames: # if there is no similar username
             user = {
             "username": username,
-            "salt": os.urandom(10),
+            "salt": salt,
             }
 
-            user["password"] = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), user.["salt"], 1000) # hashing the password
+            user["password"] = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), user["salt"], 1000) # hashing the password
 
-            self.cursor.execute("INSERT INTO login_credentials (username, salt, password) "
-                + f"VALUES ({user["username"]}, {user["salt"]}, {user["password"]});")
+            self.database.cursor.execute(f'INSERT INTO login_credentials (username, salt, password) VALUES ({user["username"]}, {user["salt"]}, {user["password"]});')
 
             self.database.connection.commit()
 
@@ -64,3 +66,7 @@ class Login:
         else:
             print("Sorry, that username is already in use")
             return False # returns False if the credentuals haven't been created
+
+if __name__=='__main__':
+    log_object = Login()
+    print(log_object.new_account())

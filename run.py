e34a7e16a23a2341ce4e12a5d0ed51e4665ffc43
Ben <benranson5@gmail.com>
2021-04-06 11:01:03 +0100

    done a boat load of commenting

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+# These lists will eventually contain Passenger and Flight objects
 list_passengers = []
 list_flights = []
 
 
+# This prints the main menu
 def print_main_menu():
     print(f"\n1. Passengers\n"
           f"2. Flights\n"
@@ -11,14 +13,25 @@ def print_main_menu():
     pass
 
 
+# This handles the input for the main menu
+# it sets the flag variable for use in deciding what menu to display
 def handle_main_menu(num):
     global running
+    global flag
+    # These values are the same as the menu selection values
     if num == 0:
         running = False
-        return
+    elif num == 1:
+        flag = "passengers"
+    elif num == 2:
+        flag = "flights"
+    elif num == 3:
+        flag = "aircraft"
+    elif num == 4:
+        flag = "staff"
 
 
-# Universal input manager
+# Universal input manager, takes an input message and an end index and returns the number entered as an int
 def num_input(input_msg, end_index):
     user_input = input(input_msg)
     while not user_input.isdigit() or int(user_input) > end_index:
@@ -26,11 +39,30 @@ def num_input(input_msg, end_index):
     return int(user_input)
 
 
+# This is the running code
 if __name__ == "__main__":
+    # This decides if the while loop is running
     global running
     running = True
 
+    # This decides which menu to display
+    global flag
+    flag = "main"
+
     while running:
-        print_main_menu()
-        user_in = num_input("Please select an option between 0 and 4:\n", 4)
-        handle_main_menu(user_in)
+        if flag == "main":  # If main then we are on the main menu
+            print_main_menu()
+            user_in = num_input("Please select an option between 0 and 4:\n", 4)
+            handle_main_menu(user_in)
+        elif flag == "passengers":  # passengers menu
+            print("p")
+            flag = "main"
+        elif flag == "flights":  # flights menu
+            print("f")
+            flag = "main"
+        elif flag == "aircraft":  # aircraft menu
+            print("a")
+            flag = "main"
+        elif flag == "staff":  # staff menu
+            print("s")
+            flag = "main"

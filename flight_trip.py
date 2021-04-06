648a240599d99cf85e2a0ae5b064114c3944493c
isobelfc <isobelfc@gmail.com>
2021-04-06 10:22:25 +0100

    flight trip outline

---
+++ flight_trip.py
@@ -0,0 +1,17 @@
+class FlightTrip:
+    def __init__(self, destination, origin):
+        self.ticket_price = None
+        self.aircraft = None
+        self.duration = None
+        self.destination = destination
+        self.origin = origin
+        self.passenger_list = None
+
+    def flight_attendees_list_report(self):
+        pass
+
+    def add_passenger(self):
+        pass
+
+    def assign_plane(self):
+        pass

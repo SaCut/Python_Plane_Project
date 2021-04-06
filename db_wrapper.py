13686a239ee5624477fecef927cd800dac7d3bc7
Ben <benranson5@gmail.com>
2021-04-06 13:33:08 +0100

    removed run import to avoid circular imports

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,45 +1,47 @@
 import pyodbc
 from people.passenger import Passenger
-import run
 
 
 class DbWrapper:
 
     # Opens a file called server_info.cfg and pulls connection info from there
     def __init__(self):
-        file_lines = open("server_info.cfg", "r").readlines()
-        self.ip = file_lines[0].strip("\n")
-        self.uname = file_lines[1].strip("\n")
-        self.password = file_lines[2].strip("\n")
-        self.DbName = file_lines[3].strip("\n")
+        try:
+            file_lines = open("server_anfo.cfg", "r").readlines()
+            self.ip = file_lines[0].strip("\n")
+            self.uname = file_lines[1].strip("\n")
+            self.password = file_lines[2].strip("\n")
+            self.DbName = file_lines[3].strip("\n")
 
-        self.connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};' +
-                                         f'SERVER={self.ip};DATABASE={self.DbName};'
-                                         f'UID={self.uname};PWD={self.password}')
+            self.connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};' +
+                                             f'SERVER={self.ip};DATABASE={self.DbName};'
+                                             f'UID={self.uname};PWD={self.password}')
 
-        self.cursor = self.connection.cursor()
+            self.cursor = self.connection.cursor()
+        except FileNotFoundError:
+            print(f"server_info.config not found!")
+            exit(1)
 
     # Will be used to pull information from the database
-    def load_all_passengers(self):
-        run.list_passengers = []
-        run.list_flights = []
+    def load_all_passengers(self, list_passengers):
+        list_passengers = []
         self.cursor.execute("SELECT * FROM passengers")
         temp_passenger_list = self.cursor.fetchall()
         for val in temp_passenger_list:
             passenger = Passenger()
             passenger.make_from_db(val[0], val[1], val[2], val[3], val[4])
-            run.list_passengers.append(passenger)
+            list_passengers.append(passenger)
 
-        for passenger in run.list_passengers:
+        for passenger in list_passengers:
             print(f"{passenger.pid} {passenger.first_name} {passenger.last_name}")
 
     # Will be used to store information in the database
-    def save_all_passengers(self):
+    def save_all_passengers(self, list_passengers):
         passenger = Passenger()
         passenger.make_from_db(None, "Isobel", "Fitt-Conway", "aasdasdas", "98989922")
-        run.list_passengers.append(passenger)
+        list_passengers.append(passenger)
 
-        for passenger in run.list_passengers:
+        for passenger in list_passengers:
             check_psg = self.cursor.execute(f"SELECT * FROM passengers WHERE passport_number = "
                                             + f"{passenger.passport_number}")
             check_psg = check_psg.fetchall()
@@ -71,4 +73,4 @@ class DbWrapper:
 
 if __name__ == "__main__":
     db = DbWrapper()
-    db.load_fresh_from_db()
+    db.load_all_passengers()

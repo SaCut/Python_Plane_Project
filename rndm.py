0eef0fc917675b3697bf308e521ebf88f92adee2
Saverio <SaCut@users.noreply.github.com>
2021-04-07 23:52:57 +0100

    updated flask files, added templates and static folders

---
+++ rndm.py
@@ -0,0 +1,15 @@
+from login import Login
+
+object = Login()
+
+# object.hash_password("admin", "admin")
+# object.right_password("admin", "admin")
+print(object.right_password("admin", object.hash_password("admin", "admin")))
+
+# hash_password:
+"bkhnxe3nxe3nxb1nxc2nxb2nxe3nxf7nx94]nx0cZnxadnx08lnxecnxe9nxb0S]Vnxbenxa8,nxd7nxb3nx18nx97-nnUnxe6k"
+# azure:
+"bkhnxe3nxe3nxb1nxc2nxb2nxe3nxf7nx94]nx0cZnxadnx08lnxecnxe9nxb0S]Vnxbenxa8,nxd7nxb3nx18nx97-nnUnxe6k"
+"bknxd17nxf5nxe4nxefnx1cnxd3nxc7vttnxf6nx83nxc9nx19nxcbonxddnxe8nxccnxa3nxffnxa30nxcenx8f,nxb7`ntnx9enx96k"
+
+

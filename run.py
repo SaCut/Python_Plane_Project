ab06aaf55d552728a6978a5f33863115d07cb43e
OneOverCosine <TheCandyDragon@gmail.com>
2021-04-07 13:41:57 +0100

    minimal changes

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from db_wrapper import DbWrapper
 from login import Login
 
 # run login method
-# login_object = Login()
-# if login_object.attempt_login():
-#     pass
-# else:
-#     raise Exception("Failed login attempt. Contact your neighbourhood coder.")
+login_object = Login()
+if login_object.attempt_login():
+    pass
+else:
+    raise Exception("Failed login attempt. Contact your neighbourhood coder.")
 
-# These lists will eventually contain Passenger and Flight objects
+# These dictionaries will eventually contain Passenger and Flight objects
 dict_passengers = {}  # Key passenger_id: val Passenger()
 dict_flights = {}  # Key flight_id: val FlightTrip()
 aircraft = {}  # Key aircraft_id: val Plane() or Helicopter()

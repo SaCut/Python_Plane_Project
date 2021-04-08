990062d3745f811643970d9012de09564447cf14
Saverio <SaCut@users.noreply.github.com>
2021-04-08 13:03:19 +0100

    updated flight_info.html

--- rndm.py
+++ rndm.py
@@ -1,10 +1,3 @@
-from db_wrapper import DbWrapper
+dictnr = {1: "lol"}
 
-db_wrapper = DbWrapper()
-dict_passengers = db_wrapper.load_all_passengers()
-dict_flights = db_wrapper.load_all_flights(dict_passengers)
-dict_aircraft = db_wrapper.load_all_aircraft()
-dict_staff = db_wrapper.load_all_staff()
-
-for i in dict_passengers.values():
-    print(dir(i))
+print(dictnr[1])
